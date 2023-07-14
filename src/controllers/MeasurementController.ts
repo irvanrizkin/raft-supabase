07b3e67ebfcdf4aa46ecadfa84682e9eb20a9be0
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-07-14 23:14:37 +0700

    feat: list measurement for last 30 min

--- src/controllers/MeasurementController.ts
+++ src/controllers/MeasurementController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { Controller } from "./Controller";
+import { sub } from "date-fns";
 
 export class MeasurementController extends Controller {
   constructor() {
@@ -16,6 +17,26 @@ export class MeasurementController extends Controller {
       return res.status(201).json({
         status: true,
         message: 'measurement added successfully',
+        results: data,
+      })
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  listByPeriod = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    try {
+      const { data, error } = await this.supabase.from('measurements')
+        .select()
+        .eq('deviceId', id)
+        .gte('createdAt', sub(new Date(), { minutes: 30 }).toISOString())
+        .order('createdAt');
+
+      return res.status(200).json({
+        status: true,
+        message: 'measurement listed successfully',
         results: data,
       })
     } catch (error) {
